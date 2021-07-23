5318158d8a3cd4f516869c4129f9c6c973921fdd
M-Manas-s <manastiwari111222@gmail.com>
2021-07-23 21:19:01 +0530

    MapView UI + Backend

--- lib/Constants/Constants.dart
+++ lib/Constants/Constants.dart
@@ -1,5 +1,27 @@
+import 'dart:math';
+
+import 'package:breathe/Classes/Vendor.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+List<Vendor> vendorList = [];
+LatLng userLoc;
+
+double calculateDistance(LatLng p1, LatLng p2) {
+
+  double lat1 = p1.latitude;
+  double lon1 = p1.longitude;
+  double lat2 = p2.latitude;
+  double lon2 = p2.longitude;
+
+  var p = 0.017453292519943295;
+  var c = cos;
+  var a = 0.5 - c((lat2 - lat1) * p) / 2 +
+      c(lat1 * p) * c(lat2 * p) *
+          (1 - c((lon2 - lon1) * p)) / 2;
+  return 12742 * asin(sqrt(a));
+}
 
 String passwordValidator(value) {
 
--- lib/Screens/MapView.dart
+++ lib/Screens/MapView.dart
@@ -1,14 +1,15 @@
 import 'dart:async';
 
+import 'package:breathe/Classes/CustomCard.dart';
+import 'package:breathe/Constants/Constants.dart';
 import "package:flutter/material.dart";
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'Login.dart';
 
 class MapView extends StatefulWidget {
-  
   static String id = 'MapViewView';
-  
+
   @override
   _MapViewState createState() => _MapViewState();
 }
@@ -16,63 +17,257 @@ class MapView extends StatefulWidget {
 class _MapViewState extends State<MapView> {
   Completer<GoogleMapController> _controller = Completer();
   bool infovis = false;
-  String Vname = "";
-  String Vpno = "";
+  int selIndex;
+  double ratio = 0.87;
 
-  void assignMarker(){
-    setState(() {
-
-    });
+  void assignMarker() {
+    setState(() {});
   }
-
-
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
 
   void _onMapViewCreated(GoogleMapController controller) {
     _controller.complete(controller);
   }
 
+  void activeIndex(int index) {
+    setState(() {
+      setState(() {
+        infovis = true;
+        selIndex = index;
+        ratio = 0.35;
+      });
+    });
+  }
+
+  List<Marker> makeSet() {
+    List<Marker> list = [
+      Marker(
+        markerId: MarkerId('You'),
+        onTap: () {
+          setState(() {
+            infovis = false;
+            ratio = 0.87;
+          });
+        },
+        position: userLoc,
+        infoWindow: InfoWindow(
+          title: 'You',
+        ),
+      ),
+    ];
+    for (int i = 0; i < vendorList.length; i++) {
+      list.add(Marker(
+        markerId: MarkerId('V$i'),
+        onTap: () {
+          activeIndex(i);
+        },
+        position: vendorList[i].location,
+        infoWindow: InfoWindow(
+            title: vendorList[i].name,
+            snippet: "${vendorList[i].quantity} remaining"),
+      ));
+    }
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex : 2,
-            child: Container(
+    Size query = MediaQuery.of(context).size;
+
+    return WillPopScope(
+      onWillPop: () async {
+        setState(() {
+          infovis = false;
+          ratio = 0.87;
+        });
+        return false;
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: query.height * (1 - ratio)),
               child: GoogleMap(
                 onMapCreated: _onMapViewCreated,
-                markers: {
-                  Marker(markerId: MarkerId('m1'),
-                      onTap: () {
-                        setState(() {
-                            Navigator.pushNamed(context, Login.id);
-                        });
-                      },
-                    position: LatLng(45.621563, -122.677433),
-                    infoWindow: InfoWindow(
-                      title: 'Oxygen Vendor1',
-                      snippet: 'Available : 1',
-                    ),
-                  )
-                },
+                markers: makeSet().toSet(),
                 initialCameraPosition: CameraPosition(
-                  target: _center,
-                  zoom: 11.0,
+                  target: userLoc,
+                  zoom: 15.0,
                 ),
               ),
             ),
-          ),
-          infovis ? Expanded(child: Container(
-            child: Column(
-              children: [
-                Text(Vname),
-                Text(Vpno),
-              ],
-            ),
-          ), flex : 1)
-          : Container(),
-        ],
+            AnimatedContainer(
+              duration: Duration(milliseconds: 200),
+              width: query.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius:
+                    BorderRadius.all(Radius.circular(!infovis ? 0 : 40.0)),
+              ),
+              margin: EdgeInsets.only(
+                  top: query.height * (infovis ? ratio - 0.01 : ratio)),
+              padding: EdgeInsets.only(top: infovis ? 20 : 0),
+              child: Center(
+                child: !infovis
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Oxygen Supplies near you",
+                            style: TextStyle(
+                              color: Theme.of(context).accentColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            "Tap on a vendor to know more",
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                vendorList[selIndex].name,
+                                style: TextStyle(
+                                  color: Theme.of(context).accentColor,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 35,
+                                  letterSpacing: 1,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(
+                                "${calculateDistance(vendorList[selIndex].location, userLoc).toStringAsFixed(2)} km",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          CustomCard(
+                            margin: EdgeInsets.symmetric(horizontal: 30),
+                            child: Container(
+                              padding: EdgeInsets.all(25),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    vendorList[selIndex].quantity.toString(),
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 40,
+                                      letterSpacing: 1,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Cylinders Available",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 20,
+                                      letterSpacing: 1,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 40,
+                                  ),
+                                  Text(
+                                    "Rs ${vendorList[selIndex].price.toString()}",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 25,
+                                      letterSpacing: 1,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            color: Theme.of(context).accentColor,
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "Call ${vendorList[selIndex].name.split(' ')[0]}",
+                                style: TextStyle(
+                                  color: Theme.of(context).accentColor,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    vendorList[selIndex].phno.toString(),
+                                    style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                      color: Theme.of(context).accentColor,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 25,
+                                      letterSpacing: 1,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.all(5),
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Theme.of(context).accentColor,
+                                    ),
+                                    child: Icon(
+                                      Icons.phone_rounded,
+                                      color: Colors.white,
+                                      size: 20,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                              height: query.height * 0.08,
+                              width: query.width,
+                              color: Theme.of(context).accentColor,
+                              child: Center(
+                                child: Text(
+                                  "Book Now",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 25,
+                                    letterSpacing: 2,
+                                  ),
+                                ),
+                              ))
+                        ],
+                      ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
