f669c771829477e5ec1eb1271a0644f14f88c3ad
M-Manas-s <manastiwari111222@gmail.com>
2021-07-23 21:18:37 +0530

    Search Backend

--- lib/Screens/Search.dart
+++ lib/Screens/Search.dart
@@ -1,8 +1,16 @@
+import 'dart:collection';
+import 'dart:math';
+
 import 'package:breathe/Classes/CirclePainter.dart';
+import 'package:breathe/Classes/Vendor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:breathe/Constants/Constants.dart';
+import 'package:geolocator/geolocator.dart';
+import 'MapView.dart';
 
 class Search extends StatefulWidget {
 
@@ -16,24 +24,81 @@ class _SearchState extends State<Search> with TickerProviderStateMixin {
   AnimationController _controller;
   Color color = Colors.blue[700];
   double size = 90.0;
-  
-  readData() {
-    //TODO: Read LatLng
+
+  Future<void> _getCurrentPosition() async {
+    final GeolocatorPlatform _geolocationPlatform = GeolocatorPlatform.instance;
+    final position = await _geolocationPlatform.getCurrentPosition();
+    userLoc= LatLng(position.latitude, position.longitude);
+    print(userLoc);
+  }
+
+
+  readData() async {
+
+    await FirebaseFirestore.instance
+        .collection('Vendor')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        if ( doc['Location']!='null' && doc['Quantity']>0 )
+        vendorList.add(
+            Vendor(
+                name: doc['Name'],
+                phno: int.parse(doc['PhoneNumber']),
+                email: doc['Email'],
+                price: (doc['Price']* 1.00),
+                location: stringToLatLng(doc['Location']),
+                quantity: doc['Quantity']
+            )
+        );
+      });
+    });
+    print(calculateDistance(vendorList[0].location, userLoc));
+  }
+
+  LatLng stringToLatLng(String str) {
+    LatLng loc = LatLng(double.parse(str.split(',')[0]), double.parse(str.split(',')[1]));
+    return loc;
+  }
+
+  closestTen() {
+    vendorList.sort((a, b) =>
+        (calculateDistance(a.location, userLoc)).compareTo(
+            calculateDistance(b.location, userLoc)));
+    while(vendorList.length > 10)
+      vendorList.removeLast();
+  }
+
+  void loadPreRequisites () async {
+    await _getCurrentPosition();
+    await readData();
+    closestTen();
+    Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MapView()
+        ),
+        ModalRoute.withName(MapView.id)
+    );
   }
 
   @override
-  void initState() {
+  void initState(){
     super.initState();
     _controller = AnimationController(
-      duration: const Duration(milliseconds: 1000),
+      duration: const Duration(milliseconds: 700),
       vsync: this,
-    )..repeat();
+    )
+      ..repeat();
+    loadPreRequisites();
   }
+
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
   }
+
   Widget _button() {
     return Center(
       child: ClipRRect(
